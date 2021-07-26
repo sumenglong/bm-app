b1c84b5719e5fa0dd05b72f8f031ba53394eaadf
sumenglong <1492463057@qq.com>
2021-07-26 10:55:42 +0800

    新闻详情

--- pages/info/info.vue
+++ pages/info/info.vue
@@ -2,17 +2,19 @@
 	<view class="content">
 		<view class="title">{{title}}</view>
 		<view class="art-content">
+			<image :src="images"></image>
 			<rich-text class="richText" :nodes="strings"></rich-text>
 		</view>
 	</view>
 </template>
 
 <script>
+	const db = uniCloud.database()
 	export default {
 		data() {
 			return {
 				title:'',
-				// titles:'',
+				images:'',
 				strings:''
 			};
 		},
@@ -20,23 +22,52 @@
 		onLoad:function(e){
 			//console.log(JSON.stringify(e));
 			//this.titles= e.title
-			uni.setNavigationBarTitle({
+			 uni.setNavigationBarTitle({
 				title:"res.data.title"
-			});
-			uni.request({
-				url: 'https://unidemo.dcloud.net.cn/api/news/36kr/'+e.newsid,
-				method: 'GET',
-				data: {},
-				success: res => {
-					this.title =res.data.title;
-					uni.setNavigationBarTitle({
-						title:res.data.title
-					});
-					this.strings =res.data.content;
-				},
-				fail: () => {},
-				complete: () => {}
-			});		
+			 });
+			
+			this.ll(e.newsid)
+			// uni.request({
+			// 	url: 'https://unidemo.dcloud.net.cn/api/news/36kr/'+e.newsid,
+			// 	method: 'GET',
+			// 	data: {},
+			// 	success: res => {
+			// 		this.title =res.data.title;
+			// 		uni.setNavigationBarTitle({
+			// 			title:res.data.title
+			// 		});
+			// 		this.strings =res.data.content;
+			// 	},
+			// 	fail: () => {},
+			// 	complete: () => {}
+			// });	
+				
+		},
+		methods: {
+			ll(newsid){
+				db.collection('news')
+				  .field('_id,news_name,news_title,news_content,news_img,create_date')
+				   .where("_id=='"+newsid+"'")
+				  .get()
+				  .then(res => {
+					  const list=res.result.data
+					  //console.log(list)
+					  this.title= res.result.data[0].news_name
+					  this.strings =res.result.data[0].news_content
+					  this.images =res.result.data[0].news_img
+					  uni.setNavigationBarTitle({
+					  			title: this.title
+					   		});
+					 
+				  }).catch(err => {
+					    uni.showModal({
+					    	content: err.message || '请求服务失败',
+					    	showCancel: false
+					    })
+					})	
+				
+			}
+			
 		}
 	}
 </script>
--- pages/new/new.vue
+++ pages/new/new.vue
@@ -1,12 +1,12 @@
 <template>
 	<view class="center">
 		<view class="uni-list">
-			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in news" :key="index" @tap="openNew" :data-newsid="item.post_id">
+			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in news" :key="index" @tap="openNew" :data-newsid="item._id">
 				<view class="uni-media-list">
-					<image class="uni-media-list-logo" :src="item.author_avatar"></image>
+					<image class="uni-media-list-logo" :src="item.news_img"></image>
 					<view class="uni-media-list-body">
-						<view class="uni-media-list-text-top">{{item.title}}</view>
-						<view class="uni-media-list-text-bottom uni-ellipsis">{{item.created_at}}</view>
+						<view class="uni-media-list-text-top">{{item.news_name}}</view>
+						<view class="uni-media-list-text-bottom uni-ellipsis">{{item.create_date}}</view>
 					</view>
 				</view>
 			</view>
@@ -15,6 +15,7 @@
 </template>
 
 <script>
+	const db = uniCloud.database()
 	export default {
 		data() {
 			return {
@@ -22,23 +23,46 @@
 			};
 		},
 		onLoad: function() {
-			uni.request({
-				url: 'https://unidemo.dcloud.net.cn/api/news',
-				method: 'GET',
-				data: {},
-				success: res => {
-					console.log(res);
-					console.log(JSON.stringify(res));
-					this.news = res.data;
-					
-				},
-				fail: () => {},
-				complete: () => {}
-			});
+			// uni.request({
+			// 	url: 'https://unidemo.dcloud.net.cn/api/news',
+			// 	method: 'GET',
+			// 	data: {},
+			// 	success: res => {
+			// 		console.log(res);
+			// 		console.log(JSON.stringify(res));
+			// 		this.news = res.data;
+			// 	},
+			// 	fail: () => {},
+			// 	complete: () => {}
+			// });
+			db.collection('news')
+			  .field('_id,news_name,news_title,news_img,create_date')
+			  .get()
+			  .then(res => {
+				  const list=res.result.data
+				  for(var i=0;i<list.length;i++){
+				  	var date = new Date(list[i].create_date);
+				  	var year = date.getFullYear();
+				  	var mon  = date.getMonth()+1;
+				  	var day  = date.getDate();
+				  	var hours = date.getHours();
+				  	var minu = date.getMinutes();
+				  	var sec = date.getSeconds();
+				  	var trMon = mon<10 ? '0'+mon : mon
+				  	var trDay = day<10 ? '0'+day : day
+				  	list[i].create_date=year+'-'+trMon+'-'+trDay+' '+hours+":"+minu+":"+sec
+				  }
+			this.news=res.result.data
+			  }).catch(err => {
+				    uni.showModal({
+				    	content: err.message || '请求服务失败',
+				    	showCancel: false
+				    })
+				})	
+			
 		},
 		methods: {
 			openNew(e) {
-				console.log(JSON.stringify(e));
 				var newsid = e.currentTarget.dataset.newsid;
 				uni.navigateTo({
 					// url: '../new/new-info',
